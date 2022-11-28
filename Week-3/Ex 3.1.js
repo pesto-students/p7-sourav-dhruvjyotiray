d48de5ac7bcd1885c621bc5dea992ab98a65091b
Dhruvjyoti Ray <dhrubojyoti21@gmail.com>
2022-11-28 12:24:55 +0530

    Adding Week-3 assignments

--- Week-3/Ex 3.1.js
+++ Week-3/Ex 3.1.js
@@ -8,15 +8,22 @@ function add(...args) {
 function memoize(fn) {
   const cache = new Map();
   return function (...args) {
+    console.log(cache) //cache display
     let key = `${args}`;
     if (cache.has(key)) {
+      console.log("Called from cache"); //for testing
       return cache.get(key);
     } else {
       cache.set(key, fn(...args));
-
+      console.log("Added to cache"); //for testing
       return cache.get(key);
     }
   };
 }
 
 const memoizeAdd = memoize(add);
+
+console.log(memoizeAdd(100, 100))
+console.log(memoizeAdd(100))
+console.log(memoizeAdd(100, 200))
+console.log(memoizeAdd(100, 100))
